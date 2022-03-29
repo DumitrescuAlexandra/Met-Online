a3e2696500850c2ca0b78ddb12ab0514d5bbd396
Alexandra <xensandra3@gmail.com>
2022-03-29 17:05:10 +0300

    Home - done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import classes from "./Home.module.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Fragment>
+      {" "}
+      <div className={classes.imageContainer}>
+        <img src="/images/met.jpg" alt="" />
+      </div>
+      <div className={classes.textBox}>
+        <div className={classes.actualText}>
+          <p>
+            Dare to <span> know </span> more. To <span> explore </span>. To
+            &nbsp;{" "}
+          </p>
+          <Link to="/discover"> discover </Link>
+          <span>{">"} </span>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Home;

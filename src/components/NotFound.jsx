8f408172d3201d8c80296f4608de0a5ed4aa11e4
Alexandra <xensandra3@gmail.com>
2022-03-30 13:59:47 +0300

    NotFound - done

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./NotFound.module.css";
+import Copyright from "./CopyRight";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className={classes.notFoundPage}>
+      <div className={classes.notFoundTitle}> 404 - Page not found</div>
+      <div className={classes.imageContainer}>
+        <img src="/images/ooops.png" alt="" />
+      </div>
+      <div className={classes.redirectLink}>
+        Go back&nbsp; <Link to="/home"> home -{">"} </Link>
+      </div>
+      <Copyright />
+    </div>
+  );
 }
 
 export default NotFound;
